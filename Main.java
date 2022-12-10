031b53ab0d3ebf89dca33e98ff65b7783e20f1b8
Sachin S B <sbsachin035@gmail.com>
2022-12-10 12:12:14 +0530

    1.0

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int sub(int a, int b) {
+        return a - b;
+    }
+}
